0b37fe95e81a041f7c196bebbfe04dfabd6c0adb
Pavol Krajkovič <xkrajkovic@stuba.sk>
2021-01-26 17:34:13 +0100

    init

--- datasets.py
+++ datasets.py
@@ -1,10 +1,27 @@
 import seaborn as sns
-
+import tkinter
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.datasets import fetch_20newsgroups
 import matplotlib.pyplot as plt
 
 
 groups = fetch_20newsgroups()
-sns.distplot(groups.target)
+sns.displot(groups.target)
 
-plt.show()
+# distribution of classes
+# plt.show()
+
+
+### BoW model - bag of words ###
+
+# initialize count vectorizer
+count_vector = CountVectorizer(max_features=500)
+
+# capture the top 500 and generate a token count matrix
+data_count = count_vector.fit_transform(groups.data)
+
+# print top 500 features uncleaned -> most popular tokens are numbers!
+# print(count_vector.get_feature_names())
+
+
+
